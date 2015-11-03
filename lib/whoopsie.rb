4fe9ce8b2fadfe36c19f2c20b6f8bbf14224b270
Wojtek Kruszewski <wojtek@oxos.pl>
2015-11-03 09:17:57 +0100

    Add report and swallow

--- lib/whoopsie.rb
+++ lib/whoopsie.rb
@@ -3,4 +3,9 @@ require "whoopsie/engine"
 require "whoopsie/railtie"
 
 module Whoopsie
+  def self.report_and_swallow
+    yield
+  rescue StandardError => e
+    ExceptionNotifier.notify_exception(e)
+  end
 end
