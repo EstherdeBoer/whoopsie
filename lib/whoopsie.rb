b5a1d709c09a38a232af6e6bfdab6ba325b2862a
Wojtek Kruszewski <wojtek@oxos.pl>
2015-11-05 11:29:51 +0100

    Add handle_exception

--- lib/whoopsie.rb
+++ lib/whoopsie.rb
@@ -6,6 +6,14 @@ module Whoopsie
   def self.report_and_swallow
     yield
   rescue StandardError => e
-    ExceptionNotifier.notify_exception(e)
+    handle_exception(e)
+  end
+
+  def self.handle_exception(exception, data = {})
+    if Rails.application.config.whoopsie.enable
+      ExceptionNotifier.notify_exception(exception, data)
+    else
+      raise exception
+    end
   end
 end
